b2bb5a55689e8d5444510cb330c92f6160a7b797
tech-compass-takahashi <tech.compass.takahashi@gmail.com>
2019-11-20 11:06:39 +0900

    課題提出JAVA－JAVAの基礎

---
+++ basic/src/basic/Kadai1.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class Kadai1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        System.out.println("初めての課題");
+        System.out.println("java攻略の道");
+	}
+
+}
---
+++ basic/src/basic/Kadai2.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class Kadai2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        /*コメント
+          コメント
+         */
+		System.out.println("hogehoge");
+		System.out.println("hogehoge2");
+	}
+
+}
