355d21f0ae132c5826b2df8838094d2c9afd1c72
tech-compass-takahashi <tech.compass.takahashi@gmail.com>
2019-12-09 14:08:54 +0900

    課題再提出JAVA-インスタンス＆クラス

--- instance/src/instance/Person.java
+++ instance/src/instance/Person.java
@@ -5,8 +5,8 @@ public class Person {
 	int age = 25;
 	//下記、課題３指示に従い追加
 	public void myProfile() {
-		System.out.println("私の名前は"+this.name+"です。");
-		System.out.println("私の年齢は"+this.age+"です。");
+		System.out.println("私の名前は" + this.name + "です。");
+		System.out.println("私の年齢は" + this.age + "です。");
 		System.out.println("よろしくお願いします。");
 	}
 }
