dbb9c5fd52336cf46549a2e297d6db54ddb4fdb2
tech-compass-takahashi <tech.compass.takahashi@gmail.com>
2019-12-18 17:49:05 +0900

    オブジェクト指向JAVA-再提出

--- orientation/src/orientation/Person.java
+++ orientation/src/orientation/Person.java
@@ -2,22 +2,22 @@ package orientation;
 
 public class Person {
 
+	private String name = "";
+	private int age = 0;
 	static int wallet = 0;
-	private String name;
-	private int age;
 
 	public String getName() {
 		return this.name;
 	}
 
-	public void setName() {
+	public void setName(String name) {
 		this.name = name;
 	}
 	public int getAge() {
 		return this.age;
 	}
 
-	public void setAge() {
+	public void setAge(int age) {
 		this.age = age;
 	}
 
@@ -26,16 +26,13 @@ public class Person {
 
 	}
 
-	Person (String name){
-		this.name = "高橋義明";
-		this.age = 25;
+	Person (String name,int age){
+		this.name = name;
+		this.age = age;
 	}
 
-	Person (){
-		this("ダミー");
-	}
+	Person (){}
 
-	//プロフィールメソッド
 	public void myProfile(){
 		System.out.println("私の名前は" + this.name + "です。");
 		System.out.println("私の年齢は" + this.age + "です。");
